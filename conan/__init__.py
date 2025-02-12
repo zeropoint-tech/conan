fca973c045a079c98c9c9f4cb8ccc06ec01208fe
czoido <mrgalleta@gmail.com>
2025-02-12 15:12:11 +0100

    Conan 2.12.2

--- conan/__init__.py
+++ conan/__init__.py
@@ -1,5 +1,5 @@
 from conan.internal.model.conan_file import ConanFile
 from conan.internal.model.version import Version
 
-__version__ = '2.12.1'
+__version__ = '2.12.2'
 conan_version = Version(__version__)
