69d4cefec057df6c718a3aa5b97b3217a046b61e
Uilian Ries <uilianries@gmail.com>
2025-02-18 18:05:27 +0100

    Add tests for chmod
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/tools/files/files.py
+++ conan/tools/files/files.py
@@ -267,6 +267,7 @@ def chdir(conanfile, newdir):
 def chmod(conanfile, path:str, read:bool, write:bool, execute:bool, recursive:bool=False):
     """
     Change the permissions of a file or directory. The same as the Unix command chmod, but simplified.
+    It skips in case running on Windows.
 
     :param conanfile: The current recipe object. Always use ``self``.
     :param path: Path to the file or directory to change the permissions.
@@ -275,17 +276,21 @@ def chmod(conanfile, path:str, read:bool, write:bool, execute:bool, recursive:bo
     :param execute: If ``True``, the file or directory will have execute permissions for any user.
     :param recursive: (Optional, Defaulted to ``False``) If ``True``, the permissions will be applied
     """
+    if platform.system() == "Windows":
+        conanfile.output.debug("Skipping tools.files.chmod on Windows.")
+        return
+
     if not os.path.exists(path):
         raise ConanException(f"Could not change permission: Path \"{path}\" does not exist.")
 
     def _change_permission(it_path:str):
         mode = os.stat(it_path).st_mode
-        permission_map = {
-            read: stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH,
-            write: stat.S_IWRITE | stat.S_IWGRP | stat.S_IWOTH,
-            execute: stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH
-        }
-        for enabled, mask in permission_map.items():
+        permissions = [
+            (read, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH),
+            (write, stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH),
+            (execute, stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+        ]
+        for enabled, mask in permissions:
             if enabled:
                 mode |= mask
             else:
@@ -294,7 +299,6 @@ def chmod(conanfile, path:str, read:bool, write:bool, execute:bool, recursive:bo
 
     if recursive:
         for root, _, files in os.walk(path):
-            _change_permission(root)
             for file in files:
                 _change_permission(os.path.join(root, file))
     else:
